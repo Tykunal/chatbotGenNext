97b68500b08865a141663fe86cc49d218608ebfe
Tykunal <kunal2021CSAI065@abesit.edu.in>
2024-11-29 13:24:17 +0530

    Added port number

--- app.py
+++ app.py
@@ -371,4 +371,4 @@ def logOut():
 
 if __name__ == '__main__':
     # app.run(debug=True) #while dealing with local development
-    app.run()
+    app.run(host='0.0.0.0', port=5000) #for Production
